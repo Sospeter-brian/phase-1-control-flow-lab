56a21d5210ebcf392ab1fb2ad197d6e99ab98b79
sospeterbrian029@gmail.com <sospeterbrian029@gmail.com>
2022-06-07 09:00:56 +0300

    control flow lab

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  const feet = 199;
+  if (feet <= 400){
+return 'This one is on me!';
+  } 
+  const feet =2001;
+  else if(feet > 2000){
+    return 'I will gladly take your thirty bucks.';
+  } else (feet > 2500){
+    return 'No can do.';
+  }
   // Write your code here!
+
 }
 
 function ternaryCheckCity(){
